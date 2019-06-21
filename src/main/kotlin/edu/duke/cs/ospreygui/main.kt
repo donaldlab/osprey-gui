13a7c42534f076735848d744c52244d6ae509b7a
Jeff Martin <jeff@cuchazinteractive.com>
2019-06-21 17:38:32 -0400

    add bond guessing based on covalent radii

--- src/main/kotlin/edu/duke/cs/ospreygui/main.kt
+++ src/main/kotlin/edu/duke/cs/ospreygui/main.kt
@@ -8,9 +8,13 @@ import edu.duke.cs.molscope.gui.Window
 import edu.duke.cs.molscope.gui.WindowCommands
 import edu.duke.cs.molscope.gui.WindowFeature
 import edu.duke.cs.molscope.molecule.*
-import edu.duke.cs.molscope.view.SpaceFilling
+import edu.duke.cs.molscope.view.BallAndStick
 import edu.duke.cs.osprey.structure.PDBIO
+import edu.duke.cs.ospreygui.prep.guessBonds
+import edu.duke.cs.osprey.structure.Molecule as OspreyMolecule
+import edu.duke.cs.osprey.structure.Atom as OspreyAtom
 import java.io.File
+import java.util.*
 
 
 fun main() = autoCloser {
@@ -19,39 +23,13 @@ fun main() = autoCloser {
 	val ospreyMol = PDBIO.readFile(File("/home/jeff/dlab/osprey3/test-resources/1CC8.ss.pdb"))
 	println("loaded ${ospreyMol.residues.size} residues")
 
-	// TODO: convert the molecule to molscope format
+	// convert the molecule to molscope format
+	val mol = ospreyMol.toMolscope("1CC8")
 
-	// make an N-terminal alanine molecule
-	val mol = Molecule(
-		"N-terminal Alanine",
-		Atoms().apply {
-			add(Atom(Element.Nitrogen, "N",   14.699, 27.060, 24.044)) // 0
-			add(Atom(Element.Hydrogen, "H1",  15.468, 27.028, 24.699)) // 1
-			add(Atom(Element.Hydrogen, "H2",  15.072, 27.114, 23.102)) // 2
-			add(Atom(Element.Hydrogen, "H3",  14.136, 27.880, 24.237)) // 3
-			add(Atom(Element.Carbon,   "CA",  13.870, 25.845, 24.199)) // 4
-			add(Atom(Element.Hydrogen, "HA",  14.468, 24.972, 23.937)) // 5
-			add(Atom(Element.Carbon,   "CB",  13.449, 25.694, 25.672)) // 6
-			add(Atom(Element.Hydrogen, "HB1", 12.892, 24.768, 25.807)) // 7
-			add(Atom(Element.Hydrogen, "HB2", 14.334, 25.662, 26.307)) // 8
-			add(Atom(Element.Hydrogen, "HB3", 12.825, 26.532, 25.978)) // 9
-			add(Atom(Element.Carbon,   "C",   12.685, 25.887, 23.222)) // 10
-			add(Atom(Element.Oxygen,   "O",   11.551, 25.649, 23.607)) // 11
-		},
-		Bonds().apply {
-			add(0, 1) // N-H1
-			add(0, 2) // N-H2
-			add(0, 3) // N-H3
-			add(0, 4) // N-CA
-			add(4, 5) // CA-HA
-			add(4, 6) // CA-CB
-			add(6, 7) // CB-HB1
-			add(6, 8) // CB-HB2
-			add(6, 9) // CB-HB3
-			add(4, 10) // CA-C
-			add(10, 11) // C-O
-		}
-	)
+	// guess the bonds
+	for (bond in mol.guessBonds()) {
+		mol.bonds.add(bond.a1, bond.a2)
+	}
 
 	// open a window
 	val win = Window(
@@ -83,7 +61,7 @@ fun main() = autoCloser {
 	// prepare a slide for the molecule
 	win.slides.add(Slide(mol.name).apply {
 		lock { s ->
-			s.views.add(SpaceFilling(mol))
+			s.views.add(BallAndStick(mol))
 			s.camera.lookAtEverything()
 		}
 	})
@@ -91,3 +69,35 @@ fun main() = autoCloser {
 	win.waitForClose()
 
 } // end of scope here cleans up all autoClose() resources
+
+
+fun OspreyMolecule.toMolscope(name: String) =
+	Molecule(name).apply {
+
+		// convert the atoms
+		val atomMap = IdentityHashMap<OspreyAtom,Atom>()
+		for (ores in residues) {
+			for (oatom in ores.atoms) {
+				val atom = Atom(
+					Element.get(oatom.elementNumber),
+					oatom.name,
+					oatom.coords[0],
+					oatom.coords[1],
+					oatom.coords[2]
+				)
+				atomMap[oatom] = atom
+				atoms.add(atom)
+			}
+		}
+
+		// convert the bonds
+		for (ores in residues) {
+			for (oatom in ores.atoms) {
+				val atom = atomMap[oatom]!!
+				for (bondedOatom in oatom.bonds) {
+					val bondedAtom = atomMap[bondedOatom]!!
+					bonds.add(atom, bondedAtom)
+				}
+			}
+		}
+	}
