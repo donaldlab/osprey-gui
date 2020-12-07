f352f543a4737ce687a59362fe17f3604871a57e
Jeff Martin <jeff@cuchazinteractive.com>
2020-12-07 09:47:02 -0500

    add tool to remove incorrect bonds added by amber when protein chains have breaks in them

--- src/main/kotlin/edu/duke/cs/ospreygui/features/slide/BondEditor.kt
+++ src/main/kotlin/edu/duke/cs/ospreygui/features/slide/BondEditor.kt
@@ -8,6 +8,7 @@ import edu.duke.cs.molscope.gui.*
 import edu.duke.cs.molscope.gui.features.FeatureId
 import edu.duke.cs.molscope.gui.features.WindowState
 import edu.duke.cs.molscope.molecule.Atom
+import edu.duke.cs.molscope.molecule.Polymer
 import edu.duke.cs.molscope.render.HoverEffects
 import edu.duke.cs.molscope.render.MoleculeRenderEffects
 import edu.duke.cs.molscope.render.RenderEffect
@@ -30,8 +31,10 @@ class BondEditor : SlideFeature {
 
 	private val pMaxDist = Ref.of(3f)
 
-	private var selection: Selection? = null
+	private var selectedAtom: SelectedAtom? = null
 
+	private var pShowChainBreaks = Ref.of(false)
+	private var selectedChain: SelectedChain? = null
 
 	override fun menu(imgui: Commands, slide: Slide.Locked, slidewin: SlideCommands) = imgui.run {
 		if (menuItem("Bonds")) {
@@ -63,7 +66,7 @@ class BondEditor : SlideFeature {
 				// did we click anything?
 				if (clickTracker.clicked(slidewin)) {
 
-					selection = null
+					selectedAtom = null
 					renderEffects.values.forEach { it.clear() }
 
 					// select the atom from the click, if any
@@ -76,7 +79,7 @@ class BondEditor : SlideFeature {
 					}
 				}
 
-				// draw the window
+				// draw the main window
 				window("Bond Editor##${slide.name}", winState.pOpen, IntFlags.of(Commands.BeginFlags.AlwaysAutoResize)) {
 
 					text("Tools:")
@@ -99,9 +102,19 @@ class BondEditor : SlideFeature {
 						|remove any extraneous bonds.
 					""".trimMargin())
 
+					if (button("Chain Breaks")) {
+						pShowChainBreaks.value = true
+					}
+					sameLine()
+					infoTip("""
+						|Sometimes the automatic bond tool will add bonds across chain breaks
+						|in a polymer, like a protein. This tool allows you to easily remove
+						|those incorrectly-added bonds.
+					""".trimMargin())
+
 					unindent(10f)
 
-					val selection = selection
+					val selection = selectedAtom
 
 					// show the selected atom
 					text("Selected:")
@@ -144,6 +157,36 @@ class BondEditor : SlideFeature {
 						}
 					}
 				}
+
+				// draw the chain breaks window, if needed
+				if (pShowChainBreaks.value) {
+					window("Bond Editor - Chain Breaks##${slide.name}", pShowChainBreaks, IntFlags.of(Commands.BeginFlags.AlwaysAutoResize)) {
+
+						tabBar("polymers") {
+
+							for ((moli, view) in views.withIndex()) {
+								val mol = view.currentMol as? Polymer ?: continue
+
+								tabItem("$mol##polymer$moli") {
+
+									for (chain in mol.chains) {
+
+										// pick a default chain if needed
+										if (mol.chains.none { it === selectedChain?.chain }) {
+											selectedChain = SelectedChain(view, mol, chain)
+										}
+
+										if (radioButton(chain.id, selectedChain?.chain === chain)) {
+											selectedChain = SelectedChain(view, mol, chain)
+										}
+									}
+
+									selectedChain?.drawChainBreaks(imgui)
+								}
+							}
+						}
+					}
+				}
 			},
 			onClose = {
 
@@ -177,7 +220,7 @@ class BondEditor : SlideFeature {
 				selectAtom(view, atom)
 			}
 
-			selection?.let { selection ->
+			selectedAtom?.let { selection ->
 				if (selection.atom !== atom) {
 
 					// get the atom info (reuse a nearby atom if possible)
@@ -194,7 +237,7 @@ class BondEditor : SlideFeature {
 		}
 	}
 
-	private fun renderBondCheck(imgui: Commands, info: Selection.AtomInfo, index: Int): Boolean = imgui.run {
+	private fun renderBondCheck(imgui: Commands, info: SelectedAtom.AtomInfo, index: Int): Boolean = imgui.run {
 
 		// add a checkbox to toggle the bond
 		if (checkbox("${info.atom.name}##atom$index", info.pBonded)) {
@@ -203,7 +246,7 @@ class BondEditor : SlideFeature {
 		return isItemHovered()
 	}
 
-	private fun renderBondLabel(imgui: Commands, info: Selection.AtomInfo) = imgui.run {
+	private fun renderBondLabel(imgui: Commands, info: SelectedAtom.AtomInfo) = imgui.run {
 
 		// show the distance
 		textColored(
@@ -233,7 +276,7 @@ class BondEditor : SlideFeature {
 	private fun selectAtom(view: MoleculeRenderView, atom: Atom) {
 
 		// make the selection for that atom
-		selection = Selection(
+		selectedAtom = SelectedAtom(
 			view,
 			atom,
 			pMaxDist.value.toDouble().square()
@@ -263,7 +306,7 @@ class BondEditor : SlideFeature {
 }
 
 
-private class Selection(val view: MoleculeRenderView, val atom: Atom, val maxDistSq: Double) {
+private class SelectedAtom(val view: MoleculeRenderView, val atom: Atom, val maxDistSq: Double) {
 
 	val mol get() = view.molStack.originalMol
 	val bondGuesser = BondGuesser()
@@ -272,15 +315,15 @@ private class Selection(val view: MoleculeRenderView, val atom: Atom, val maxDis
 
 		constructor(atom: Atom) : this(
 			atom,
-			this@Selection.atom.pos.distance(atom.pos)
+			this@SelectedAtom.atom.pos.distance(atom.pos)
 		)
 
-		val pBonded = Ref.of(mol.bonds.isBonded(this@Selection.atom, atom))
+		val pBonded = Ref.of(mol.bonds.isBonded(this@SelectedAtom.atom, atom))
 
-		val covalentRange = atom.covalentRange(this@Selection.atom, bondGuesser)
+		val covalentRange = atom.covalentRange(this@SelectedAtom.atom, bondGuesser)
 
 		fun updateBond() {
-			val a1 = this@Selection.atom
+			val a1 = this@SelectedAtom.atom
 			val a2 = this@AtomInfo.atom
 			if (pBonded.value) {
 				mol.bonds.add(a1, a2)
@@ -304,6 +347,116 @@ private class Selection(val view: MoleculeRenderView, val atom: Atom, val maxDis
 		.toList()
 		.sortedBy { it.dist }
 }
+
+
+private class SelectedChain(val view: MoleculeRenderView, val mol: Polymer, val chain: Polymer.Chain) {
+
+	val bondGuesser = BondGuesser()
+
+	// find the connecting bonds for every adjacent pair of residues
+	inner class InterBond(
+		val res1: Polymer.Residue,
+		val res2: Polymer.Residue,
+		val bonds: MutableList<Bond>
+	) {
+		fun Polymer.Residue.label() = "$type $id"
+		val label = "${res1.label()} - ${res2.label()}"
+
+		fun removeBonds() {
+
+			// update the molecule
+			for (bond in bonds) {
+				mol.bonds.remove(bond.atom1, bond.atom2)
+			}
+			view.moleculeChanged()
+
+			// update this
+			bonds.clear()
+		}
+	}
+
+	inner class Bond(
+		val atom1: Atom,
+		val atom2: Atom
+	) {
+		val dist = atom1.pos.distance(atom2.pos)
+		val covalentRange = atom1.covalentRange(atom2)
+		val good = dist in covalentRange
+	}
+
+	private val interBonds: List<InterBond> = ArrayList<InterBond>().apply {
+		for (i in 1 until chain.residues.size) {
+			val res1 = chain.residues[i - 1]
+			val res2 = chain.residues[i]
+
+			val bonds = ArrayList<Bond>()
+			for (atom1 in res1.atoms) {
+				for (atom2 in mol.bonds.bondedAtoms(atom1)) {
+					if (res2.atoms.any { it === atom2 }) {
+						bonds.add(Bond(atom1, atom2))
+					}
+				}
+			}
+
+			add(InterBond(res1, res2, bonds))
+		}
+	}
+
+	fun drawChainBreaks(imgui: Commands) = imgui.run {
+
+		child("chain", 300f, 600f, true) {
+
+			for ((i, interBond) in interBonds.withIndex()) {
+
+				text(interBond.label)
+				sameLine()
+				if (button("X##$i")) {
+					interBond.removeBonds()
+				}
+				sameLine()
+				infoTip("""
+					|Remove the bonds between these residues
+				""".trimMargin())
+
+				indent(10f)
+
+				// show individual bonds
+				if (interBond.bonds.isNotEmpty()) {
+					for (bond in interBond.bonds) {
+
+						text("${bond.atom1.name} - ${bond.atom2.name}")
+						sameLine()
+						textColored(
+							if (bond.good) {
+								ColorRGBA.Int(0, 255, 0)
+							} else {
+								ColorRGBA.Int(255, 0, 0)
+							},
+							"${"%.2f".format(bond.dist)} A"
+						)
+						if (isItemHovered()) {
+							setTooltip("%.2f A %s in range [%.2f A, %.2f A]".format(
+								bond.dist,
+								if (bond.good) {
+									"is"
+								} else {
+									"is not"
+								},
+								bond.covalentRange.start,
+								bond.covalentRange.endInclusive
+							))
+						}
+					}
+				} else {
+					text("(no bonds found)")
+				}
+
+				unindent(10f)
+			}
+		}
+	}
+}
+
 
 private val hoverEffect = RenderEffect(
 	ByteFlags.of(RenderEffect.Flags.Highlight, RenderEffect.Flags.Inset, RenderEffect.Flags.Outset),
